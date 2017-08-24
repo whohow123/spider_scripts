afb524be692458ae37ad04cb3157aff89ebe37b4
hujunqi <hujunqi@cmcm.com>
2017-08-24 17:15:15 +0800

    新增入口文件

---
+++ scripts/youtube_spider.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+import os
+
+class YoutubeSpider(object):
+    """download youtube video info"""
+    def run(self):
+        pass
+
+    def youtube_search(self):
+        pass
+
+    def get_best_download_url(self):
+        # you-get
+        pass
+
+    def get_youtube_video_as_mp3(self):
+        # ffmpeg
+        pass
+
+    def get_video_caption(self):
+        # script get youtube video caption
+        pass
