783ebb826ab9c6d84520b021f08653eda93f03dd
hujunqi <hujunqi@cmcm.com>
2017-08-25 15:13:18 +0800

    新增youtube视频信息下载脚本

--- scripts/youtube_spider.py
+++ scripts/youtube_spider.py
@@ -1,22 +1,156 @@
 # encoding: utf-8
-import os
+import ffmpy
+import configure
+import json
+import asyncio
+import subprocess
+from os.path import join as pathjoin
+from html import unescape
+import requests
+from bs4 import BeautifulSoup
+import io
+import sys
+
+requests.packages.urllib3.disable_warnings()
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')
+
 
 class YoutubeSpider(object):
     """download youtube video info"""
     def run(self):
-        pass
+        search_info = {
+            'words': 'child',
+            'lan': 'en',
+            'region_code': 'US',
+            'relevance_language': 'en'
+        }
+        search_url = configure.YOUTUBE_SEARCH_URL % \
+                     (search_info['words'], configure.GOOGLEAPIS_KEY, search_info['region_code'], search_info['relevance_language'])
 
-    def youtube_search(self):
-        pass
+        video_list = self.get_youtube_search_results(search_url)
+        for video_id in video_list:
+            video_url, title = self.get_best_download_url(video_id)
+            if video_url != "" and title != "":
+                # down youtube video caption
+                res = self.get_video_caption(video_id, search_info, title)
+                if res is True:
+                    # down youtube video as mp3 format
+                    self.get_youtube_video_as_mp3(video_url, title)
+                    exit()
 
-    def get_best_download_url(self):
+    def get_youtube_search_results(self, search_url):
+        r = requests.get(search_url, verify=False)
+        data_list = r.content.decode('utf-8', 'ignore')
+        json_data_list = json.loads(data_list)
+        if json_data_list['items']:
+            id_list = []
+            for item in json_data_list['items']:
+                id_list.append(item['id']['videoId'])
+
+        return id_list
+
+    def get_best_download_url(self, video_id):
         # you-get
-        pass
+        title = ''
+        download_url = ''
+        cmd = 'you-get --json '+configure.DETAIL_YOUTUBE+video_id
+        result = json.loads(self.run_cmd(cmd))
+        if result['streams']:
+            for key, val in result['streams'].items():
+                if val['container'] == 'mp4':
+                    download_url = val['url']
+                    break
 
-    def get_youtube_video_as_mp3(self):
-        # ffmpeg
-        pass
+            title = result['title']
 
-    def get_video_caption(self):
+        return download_url, title
+
+    def get_youtube_video_as_mp3(self, video_url, title):
+        file_name = pathjoin(configure.DOWN_DIR, title+'.mp3')
+
+        url_path = video_url
+        mp3_path = file_name
+
+        try:
+            ff = ffmpy.FFmpeg(
+                inputs={url_path: None},
+                outputs={mp3_path: ' -acodec libmp3lame -vn '}
+            )
+            ff.cmd
+            ff.run()
+
+            print(title+' === success !')
+            return True
+        except ffmpy.FFRuntimeError:
+            print("MP4 to MP3 failed")
+
+    def get_video_caption(self, *args):
         # script get youtube video caption
-        pass
+        video_id = args[0]
+        search_info = args[1]
+        title = args[2]
+        
+        title = title.replace(':', ' -').replace('|', '-')
+
+        # 设置要保存.srt字幕的路径
+        des = configure.DOWN_DIR + title + '.srt'
+
+        # 获取自带字幕
+        caption_url = configure.YOUTUBE_CAPTION_URL % (search_info['lan'], video_id)
+
+        r_caps_en_xml = requests.get(caption_url, verify=False)
+        xml_content = r_caps_en_xml.content.decode('utf-8', 'ignore')
+        if xml_content:
+            soup_caps_en_xml = BeautifulSoup(xml_content)
+            list_data = soup_caps_en_xml.find_all('text')
+
+            # 将XML信息转换成.srt字幕信息并打印保存成srt文件
+            with open(des, 'w', encoding='utf-8') as f:
+                num = 1
+                for i in list_data:
+                    start_time = float(i['start'])
+                    end_time = float(i['start']) + float(i['dur'])
+                    start_time = self.sec_to_srt_format(start_time)
+                    end_time = self.sec_to_srt_format(end_time)
+                    text_line = unescape(i.text).replace('\n', ' ')
+                    mystr = """{var1} 
+                    {var2} --> {var3} 
+                    {var4}\n """.format(var1=num, var2=start_time, var3=end_time, var4=text_line)
+                    f.writelines(mystr)
+                    num += 1
+            return True
+        else:
+            print('xml is empty!')
+            return False
+
+    def run_cmd(self, cmd):
+        process = subprocess.Popen(cmd, shell=True,
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result_f, error_f = process.stdout, process.stderr
+
+        errors = error_f.read()
+        if errors:
+            return errors
+        result_str = result_f.read().strip()
+
+        if result_f:
+            result_f.close()
+        if error_f:
+            error_f.close()
+
+        return result_str
+
+    def sec_to_srt_format(self, t):
+        tm = t * 1000
+        hao = tm % 1000
+        tm = tm / 1000
+        miao = tm % 60
+        tm = tm / 60
+        fen = tm % 60
+        tm = tm / 60
+        shi = tm
+
+        return "%02d:%02d:%02d,%03d" % (shi, fen, miao, hao)
+    
+
+YoutubeSpider().run()
